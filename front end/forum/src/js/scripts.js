44e4e167b1b4b8b4215527b34bf5d3edeb7b2b26
nickchen8873 <47801761+nickchen8873@users.noreply.github.com>
2019-05-30 11:42:57 +0800

    update

--- front end/forum/src/js/scripts.js
+++ front end/forum/src/js/scripts.js
@@ -4,29 +4,3 @@ $(document).on('click', '.reply', function(){
 console.log($(document).scrollTop);
 	$(document).scrollTop(10000);
 });
-
-<script>
-
-function readURL(input){
-
-  if(input.files && input.files[0]){
-
-    var imageTagID = input.getAttribute("targetID");
-
-    var reader = new FileReader();
-
-    reader.onload = function (e) {
-
-       var img = document.getElementById(imageTagID);
-
-       img.setAttribute("src", e.target.result)
-
-    }
-
-    reader.readAsDataURL(input.files[0]);
-
-  }
-
-}
-
-</script>
