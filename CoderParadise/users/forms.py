efefaa93ac070793ce20f54f73f78c173fb721f5
DESKTOP-BDE7PNH\leoxz <leoxz02leo@outlook.com>
2019-05-30 13:47:23 +0800

    Create UserProfile

--- CoderParadise/users/forms.py
+++ CoderParadise/users/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import UserProfile
+from django.core.files.images import get_image_dimensions
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +11,41 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+# class UserProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = UserProfile
+
+#     def clean_avatar(self):
+#         avatar = self.cleaned_data['avatar']
+
+#         try:
+#             w, h = get_image_dimensions(avatar)
+
+#             #validate dimensions
+#             max_width = max_height = 100
+#             if w > max_width or h > max_height:
+#                 raise forms.ValidationError(
+#                     u'Please use an image that is '
+#                      '%s x %s pixels or smaller.' % (max_width, max_height))
+
+#             #validate content type
+#             main, sub = avatar.content_type.split('/')
+#             if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
+#                 raise forms.ValidationError(u'Please use a JPEG, '
+#                     'GIF or PNG image.')
+
+#             #validate file size
+#             if len(avatar) > (20 * 1024):
+#                 raise forms.ValidationError(
+#                     u'Avatar file size may not exceed 20k.')
+
+#         except AttributeError:
+#             """
+#             Handles case when we are updating the user profile
+#             and do not supply a new avatar
+#             """
+#             pass
+
+#         return avatar
