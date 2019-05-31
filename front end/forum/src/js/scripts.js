a00a99b943552a5cd79c0daef3cd5d8d3ed7555b
nickchen8873 <47801761+nickchen8873@users.noreply.github.com>
2019-06-01 04:24:01 +0800

    update something

--- front end/forum/src/js/scripts.js
+++ front end/forum/src/js/scripts.js
@@ -4,3 +4,36 @@ $(document).on('click', '.reply', function(){
 console.log($(document).scrollTop);
 	$(document).scrollTop(10000);
 });
+
+var fileSelect = document.getElementById("fileSelect"),
+  fileElem = document.getElementById("fileElem"),
+  fileList = document.getElementById("fileList");
+
+fileSelect.addEventListener("click", function (e) {
+  if (fileElem) {
+    fileElem.click();
+  }
+  e.preventDefault(); // prevent navigation to "#"
+}, false);
+
+function handleFiles(files) {
+  if (!files.length) {
+    fileList.innerHTML = "<p>No files selected!</p>";
+  }
+  else {
+    var list = document.createElement("ul");
+    for (var i = 0; i < files.length; i++) {
+      var li = document.createElement("li");
+      list.appendChild(li);
+      
+      var img = document.createElement("img");
+      img.src = window.URL.createObjectURL(files[i]);
+      img.height = 60;
+      img.onload = function () {
+        window.URL.revokeObjectURL(this.src);
+      }
+      li.appendChild(img);
+    }
+    fileList.appendChild(list);
+  }
+}
